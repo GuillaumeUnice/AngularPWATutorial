ecc6af8bca89fe8bb76dc9db7ed3aeffbea61252
Echyzen <guillaume.unice@gmail.com>
2018-03-17 19:21:49 +0100

    Add frontend src (call server + display logo img)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  public myData
+  constructor (private _http: HttpClient) {}
+
+  fetchData() {
+    return this._http.get('http://127.0.0.1:3000/posts/1').toPromise()
+      .then(data => {
+        this.myData = data
+      })
+      .catch(err => console.error(err))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
